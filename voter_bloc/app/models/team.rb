0cf075699614f978fdcf750b897a8291e9113c69
Mason Pierce <masopie@gmail.com>
2016-07-07 14:42:58 -0500

    Move 'has_voted?' rat's nest back to view for now

--- voter_bloc/app/models/team.rb
+++ voter_bloc/app/models/team.rb
@@ -38,8 +38,4 @@ class Team < ActiveRecord::Base
       @sorted_teams << @large
       @sorted_teams << @xlarge
   end
-
-  def has_voted?
-    
-  end
 end
--- voter_bloc/app/models/vote.rb
+++ voter_bloc/app/models/vote.rb
@@ -2,4 +2,3 @@ class Vote < ActiveRecord::Base
   belongs_to :election
   belongs_to :voter, class_name: "User"
 end
-
